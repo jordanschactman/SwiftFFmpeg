371071a1f083002338a6af83099230118f68e289
Jordan Schactman <jordanschactman@gmail.com>
2021-07-26 13:01:15 -0400

    Add arm64 support for XCFrameworks

--- Package.swift
+++ Package.swift
@@ -1,10 +1,11 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
   name: "SwiftFFmpeg",
+  platforms: [.iOS(.v9), .macOS(.v10_10), .tvOS(.v10)],
   products: [
     .library(
       name: "SwiftFFmpeg",
@@ -12,13 +13,70 @@ let package = Package(
     )
   ],
   targets: [
-    .systemLibrary(
-      name: "CFFmpeg",
-      pkgConfig: "libavformat"
-    ),
     .target(
       name: "SwiftFFmpeg",
       dependencies: ["CFFmpeg"]
+    ),
+    .target(
+      name: "CFFmpeg",
+      dependencies: [
+        "libavcodec",
+        "libavdevice",
+        "libavfilter",
+        "libavformat",
+        "libavutil",
+        "libswresample",
+        "libswscale",
+      ],
+      linkerSettings: [
+        .linkedLibrary("z"),
+        .linkedLibrary("bz2"),
+        .linkedLibrary("iconv"),
+        .linkedLibrary("lzma"),
+        .linkedFramework("Security"),
+        .linkedFramework("CoreMedia"),
+        .linkedFramework("CoreVideo"),
+        .linkedFramework("AudioToolbox"),
+        .linkedFramework("VideoToolbox"),
+        .linkedFramework("OpenGL"),
+        .linkedFramework("CoreImage"),
+        .linkedFramework("AppKit"),
+      ]
+    ),
+    .binaryTarget(
+      name: "libavcodec",
+      url: "https://github.com/jordanschactman/SwiftFFmpeg/blob/master/xcframework/libavcodec.zip?raw=true",
+      checksum: "48b75120c752a0e077ce6e3c84ea7b373b9af973088c0b406b077d666db52b5a"
+    ),
+    .binaryTarget(
+      name: "libavdevice",
+      url: "https://github.com/jordanschactman/SwiftFFmpeg/blob/master/xcframework/libavdevice.zip?raw=true",
+      checksum: "5f7e780f4dbd01272ace2dd169809506f08ae2a229db5f3a5091579bf4959f42"
+    ),
+    .binaryTarget(
+      name: "libavfilter",
+      url: "https://github.com/jordanschactman/SwiftFFmpeg/blob/master/xcframework/libavfilter.zip?raw=true",
+      checksum: "55c51b6184f5560c0327a190078e92d30cba0e03448f26b32b43e55f281a9dc9"
+    ),
+    .binaryTarget(
+      name: "libavformat",
+      url: "https://github.com/jordanschactman/SwiftFFmpeg/blob/master/xcframework/libavformat.zip?raw=true",
+      checksum: "b9b365d8b2c2c757d297d3a0ebba3b9b1b9b8a9da5a7b1434fd63c367d7262b1"
+    ),
+    .binaryTarget(
+      name: "libavutil",
+      url: "https://github.com/jordanschactman/SwiftFFmpeg/blob/master/xcframework/libavutil.zip?raw=true",
+      checksum: "088ead89b7ebfb8e25e4a70bbb795b668e3dd14fbe84f769b9d8d41ec6fca975"
+    ),
+    .binaryTarget(
+      name: "libswresample",
+      url: "https://github.com/jordanschactman/SwiftFFmpeg/blob/master/xcframework/libswresample.zip?raw=true",
+      checksum: "d474d0f8c059d9a10bd77aff09afad112009e6d06dc8659514fa947df4fdfe58"
+    ),
+    .binaryTarget(
+      name: "libswscale",
+      url: "https://github.com/jordanschactman/SwiftFFmpeg/blob/master/xcframework/libswscale.zip?raw=true",
+      checksum: "a9f8b98753879280413ada694038bf74fc3e99097551ccdfdec5813d06491dfd"
     ),
     .target(
       name: "SwiftFFmpegExamples",
--- scripts/build.sh
+++ scripts/build.sh
@@ -3,8 +3,17 @@
 FFMPEG_VERSION=4.3.1
 FFMPEG_SOURCE_DIR=FFmpeg-n$FFMPEG_VERSION
 FFMPEG_LIBS="libavcodec libavdevice libavfilter libavformat libavutil libpostproc libswresample libswscale"
-PREFIX=`pwd`/output
-ARCH="x86_64"
+
+HOST_ARCH=$(uname -m)
+HOST_OS=$(uname -s)
+
+PREFIXES=("`pwd`/output-x86_64")
+ARCHES=("x86_64")
+
+if [[ "$HOST_ARCH" == "arm64" ]]; then
+  PREFIXES+=("`pwd`/output-arm64")
+  ARCHES+=("arm64")
+fi
 
 if [ ! -d $FFMPEG_SOURCE_DIR ]; then
   echo "Start downloading FFmpeg..."
@@ -15,26 +24,195 @@ fi
 
 echo "Start compiling FFmpeg..."
 
-rm -rf $PREFIX
+for prefix in ${PREFIXES[@]}; do
+  rm -rf $prefix
+done
+
 cd $FFMPEG_SOURCE_DIR
 
-./configure \
-  --prefix=$PREFIX \
-  --enable-gpl \
-  --enable-version3 \
-  --disable-programs \
-  --disable-doc \
-  --arch=$ARCH \
-  --extra-cflags="-arch $ARCH -march=native -fno-stack-check" \
-  --disable-debug || exit 1
+for i in ${!PREFIXES[@]}; do
 
-make clean
-make -j8 install || exit 1
+  if [[ "${ARCHES[$i]}" != "$HOST_ARCH" ]]; then
+
+    ./configure --prefix=${PREFIXES[$i]} \
+      --enable-gpl \
+      --enable-version3 \
+      --disable-programs \
+      --disable-doc \
+      --enable-cross-compile \
+      --arch=${ARCHES[$i]} \
+      --extra-cflags="-fno-stack-check -target ${ARCHES[$i]}-apple-macos10.10" \
+      --extra-ldflags="-target ${ARCHES[$i]}-apple-macos10.10" \
+      --disable-debug || exit 1
+
+  else
+
+    if [[ "$HOST_OS" == "Darwin" ]]; then
+
+      ./configure --prefix=${PREFIXES[$i]} \
+        --enable-gpl \
+        --enable-version3 \
+        --disable-programs \
+        --disable-doc \
+        --arch=${ARCHES[$i]} \
+        --extra-cflags="-fno-stack-check -target ${ARCHES[$i]}-apple-macos10.10" \
+        --extra-ldflags="-target ${ARCHES[$i]}-apple-macos10.10" \
+        --disable-debug || exit 1
+
+    else
+
+      ./configure --prefix=${PREFIXES[$i]} \
+        --enable-gpl
+        --enable-version3 \
+        --disable-programs \
+        --disable-doc \
+        --arch=${ARCHES[$i]} \
+        --extra-cflags="-fno-stack-check" \
+        --disable-debug || exit 1
+
+    fi
+
+  fi
+
+  make clean
+  make -j8 install || exit 1
+
+done
 
 cd ..
 
+function build_framework() {
+
+  local LIB_NAME=$1
+
+  local LIB_XCFRAMEWORK=xcframework/$LIB_NAME.xcframework
+  local LIB_FRAMEWORKS=()
+
+  for prefix in ${PREFIXES[@]}; do
+    LIB_FRAMEWORKS+=($prefix/framework/$LIB_NAME.framework)
+  done
+
+  # build framework
+
+  for i in ${!LIB_FRAMEWORKS[@]}; do
+
+    rm -rf ${LIB_FRAMEWORKS[$i]}
+
+    mkdir -p ${LIB_FRAMEWORKS[$i]}/Headers
+    cp -R ${PREFIXES[$i]}/include/$LIB_NAME/ ${LIB_FRAMEWORKS[$i]}/Headers
+
+    cp ${PREFIXES[$i]}/lib/$LIB_NAME.a ${LIB_FRAMEWORKS[$i]}/$LIB_NAME
+
+    cat > ${LIB_FRAMEWORKS[$i]}/Info.plist << EOF
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+  <key>CFBundleDevelopmentRegion</key>
+  <string>en</string>
+  <key>CFBundleExecutable</key>
+  <string>$LIB_NAME</string>
+  <key>CFBundleIdentifier</key>
+  <string>org.ffmpeg.$LIB_NAME</string>
+  <key>CFBundleInfoDictionaryVersion</key>
+  <string>6.0</string>
+  <key>CFBundleName</key>
+  <string>$LIB_NAME</string>
+  <key>CFBundlePackageType</key>
+  <string>FMWK</string>
+  <key>CFBundleShortVersionString</key>
+  <string>$FFMPEG_VERSION</string>
+  <key>CFBundleVersion</key>
+  <string>$FFMPEG_VERSION</string>
+  <key>CFBundleSignature</key>
+  <string>????</string>
+  <key>NSPrincipalClass</key>
+  <string></string>
+</dict>
+</plist>
+EOF
+
+  done
+
+  # build xcframework
+
+  rm -rf $LIB_XCFRAMEWORK
+
+  if [[ "$HOST_ARCH" == "arm64" ]]; then
+  
+    mkdir -p $LIB_XCFRAMEWORK/macos-x86_64
+    mkdir -p $LIB_XCFRAMEWORK/macos-arm64
+    cp -R ${LIB_FRAMEWORKS[0]} $LIB_XCFRAMEWORK/macos-x86_64
+    cp -R ${LIB_FRAMEWORKS[1]} $LIB_XCFRAMEWORK/macos-arm64
+
+    cat > $LIB_XCFRAMEWORK/Info.plist << EOF
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+  <key>AvailableLibraries</key>
+  <array>
+    <dict>
+      <key>LibraryIdentifier</key>
+      <string>$LIB_NAME</string>
+      <key>LibraryPath</key>
+      <string>$LIB_NAME.framework</string>
+      <key>SupportedArchitectures</key>
+      <array>
+        <string>x86_64</string>
+        <string>arm64</string>
+      </array>
+      <key>SupportedPlatform</key>
+      <string>macos</string>
+    </dict>
+  </array>
+  <key>CFBundlePackageType</key>
+  <string>XFWK</string>
+  <key>XCFrameworkFormatVersion</key>
+  <string>1.0</string>
+</dict>
+</plist>
+EOF
+
+else
+
+  mkdir -p $LIB_XCFRAMEWORK/macos-x86_64
+  cp -R ${LIB_FRAMEWORKS[0]} $LIB_XCFRAMEWORK/macos-x86_64
+
+  cat > $LIB_XCFRAMEWORK/Info.plist << EOF
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+  <key>AvailableLibraries</key>
+  <array>
+    <dict>
+      <key>LibraryIdentifier</key>
+      <string>$LIB_NAME</string>
+      <key>LibraryPath</key>
+      <string>$LIB_NAME.framework</string>
+      <key>SupportedArchitectures</key>
+      <array>
+        <string>x86_64</string>
+      </array>
+      <key>SupportedPlatform</key>
+      <string>macos</string>
+    </dict>
+  </array>
+  <key>CFBundlePackageType</key>
+  <string>XFWK</string>
+  <key>XCFrameworkFormatVersion</key>
+  <string>1.0</string>
+</dict>
+</plist>
+EOF
+
+  fi
+
+}
+
 for LIB in $FFMPEG_LIBS; do
-  ./build_framework.sh $PREFIX $LIB $FFMPEG_VERSION || exit 1
+  build_framework $LIB || exit 1
 done
 
 echo "The compilation of FFmpeg is completed."
